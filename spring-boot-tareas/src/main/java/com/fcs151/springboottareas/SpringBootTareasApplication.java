814f759d883d6a22844589fdbb42571fad994a8e
federico.schaefer <federico.schaefer@ues21.edu.ar>
2017-12-05 18:26:42 -0300

    adding spring-boot-tareas

---
+++ spring-boot-tareas/src/main/java/com/fcs151/springboottareas/SpringBootTareasApplication.java
@@ -0,0 +1,12 @@
+package com.fcs151.springboottareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTareasApplication.class, args);
+	}
+}
---
+++ spring-boot-tareas/src/test/java/com/fcs151/springboottareas/SpringBootTareasApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fcs151.springboottareas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootTareasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
