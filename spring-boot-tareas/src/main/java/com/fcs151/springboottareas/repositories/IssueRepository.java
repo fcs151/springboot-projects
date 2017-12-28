5eda0859cd3ab8b914e21c774155181f68f7a3d0
federico.schaefer <federico.schaefer@ues21.edu.ar>
2017-12-28 18:20:13 -0300

    fixing testing

--- spring-boot-tareas/src/main/java/com/fcs151/springboottareas/repositories/IssueRepository.java
+++ spring-boot-tareas/src/main/java/com/fcs151/springboottareas/repositories/IssueRepository.java
@@ -3,9 +3,11 @@ package com.fcs151.springboottareas.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.fcs151.springboottareas.entities.Issue;
 
+@Repository
 public interface IssueRepository extends JpaRepository<Issue, Integer> {
     public List<Issue> findByResponsibleId(Integer workerId);
 
